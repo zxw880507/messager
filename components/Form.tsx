9c995adf0c970fafc97ccf2d9afcb27f3813c447
Calvin <zxw880507@gmail.com>
2021-10-02 16:34:21 -0600

    add grid to form

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,19 +1,35 @@
 import { Paper, Grid } from "@mui/material";
-import { ReactChild } from "react";
+// import { ReactChild } from "react";
+import FormItem from "./FormItem";
 import style from "./Form.module.scss";
 
-interface Props {
-  children?: ReactChild;
-}
-
-export default function Form(props: Props) {
+export default function Form() {
   return (
     <Paper elevation={3} className={style.root}>
-      <Grid container flexDirection="column">
-        <Grid item>
-          <h2 className={style.title}>Welcome to Messager</h2>
+      <Grid
+        style={{ height: "100%" }}
+        container
+        flexDirection="column"
+        justifyContent="space-evenly"
+      >
+        <Grid
+          item
+          container
+          flex={2}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <h1 className={style.title}>Welcome to Messager</h1>
         </Grid>
-        <Grid item>{props.children}</Grid>
+        <Grid
+          item
+          container
+          flex={8}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <FormItem />
+        </Grid>
       </Grid>
     </Paper>
   );
