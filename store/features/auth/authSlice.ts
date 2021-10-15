d71adce978144f2e472a469dc702f0168e3a49a4
Calvin <zxw880507@gmail.com>
2021-10-15 16:42:26 -0600

    rejectwithvalue manually

--- store/features/auth/authSlice.ts
+++ store/features/auth/authSlice.ts
@@ -1,20 +1,25 @@
 import { createSlice, createAsyncThunk, AnyAction } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { RootState } from "../..";
 import { resetInput } from "../inputsSlice";
 
 enum Status {
   idle = "idle",
   loading = "loading",
+  pending = "pending",
   succeeded = "succeeded",
   failed = "failed",
 }
 
-type AuthState = {
+interface AuthState {
   auth: Data<string | undefined> | null;
   status: keyof typeof Status;
-  error: string | null;
-};
+  error: string | null | undefined;
+}
+interface ValidationErrors {
+  errorMessage: string;
+  field_errors: Record<string, string>;
+}
 type Data<T> = {
   id: T;
   email: T;
@@ -29,28 +34,55 @@ const initialState: AuthState = {
 
 export const setLogin = createAsyncThunk(
   "auth/setLogin",
-  async (_, { getState, dispatch }) => {
+  async (_, { getState, dispatch, rejectWithValue }) => {
     const { formInputs } = getState() as RootState;
     dispatch(resetInput());
-    const res = await axios.post("api/login", formInputs);
-    return res.data;
+    try {
+      const res = await axios.post("api/login", formInputs);
+      return res.data;
+    } catch (err) {
+      let error: AxiosError<ValidationErrors> = err;
+      if (!error.response) {
+        throw err;
+      }
+      return rejectWithValue(error.response.data);
+    }
   }
 );
 
 export const setSignup = createAsyncThunk(
   "auth/setSignup",
-  async (_, { getState, dispatch }) => {
+  async (_, { getState, dispatch, rejectWithValue }) => {
     const { formInputs } = getState() as RootState;
     dispatch(resetInput());
-    const res = await axios.post("api/signup", formInputs);
-    return res.data;
+    try {
+      const res = await axios.post("api/signup", formInputs);
+      return res.data;
+    } catch (err) {
+      let error: AxiosError<ValidationErrors> = err;
+      if (!error.response) {
+        throw err;
+      }
+      return rejectWithValue(error.response.data);
+    }
   }
 );
 
-export const getUser = createAsyncThunk("auth/getUser", async () => {
-  const res = await axios.get("api/user");
-  return res.data;
-});
+export const getUser = createAsyncThunk(
+  "auth/getUser",
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await axios.get("api/user");
+      return res.data;
+    } catch (err) {
+      let error: AxiosError<ValidationErrors> = err;
+      if (!error.response) {
+        throw err;
+      }
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
 
 export const setLogout = createAsyncThunk("auth/setLogout", async () => {
   const res = await axios.get("api/logout");
@@ -60,7 +92,11 @@ export const setLogout = createAsyncThunk("auth/setLogout", async () => {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    resetError(state) {
+      state.error = null;
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(setLogin.pending, (state) => {
@@ -70,9 +106,13 @@ const authSlice = createSlice({
         state.status = "succeeded";
         state.auth = action.payload;
       })
-      .addCase(setLogin.rejected, (state, action) => {
+      .addCase(setLogin.rejected, (state, action: AnyAction) => {
         state.status = "failed";
-        state.error = action.error.message as string | null;
+        if (action.payload) {
+          state.error = action.payload.errorMessage;
+        } else {
+          state.error = action.error.message;
+        }
       })
       .addCase(setSignup.pending, (state) => {
         state.status = "loading";
@@ -81,20 +121,31 @@ const authSlice = createSlice({
         state.status = "succeeded";
         state.auth = action.payload;
       })
-      .addCase(setSignup.rejected, (state, action) => {
+      .addCase(setSignup.rejected, (state, action: AnyAction) => {
         state.status = "failed";
-        state.error = action.error.message as string | null;
+        if (action.payload) {
+          state.error = action.payload.errorMessage;
+        } else {
+          state.error = action.error.message;
+        }
       })
       .addCase(getUser.pending, (state) => {
-        state.status = "loading";
+        state.status = "pending";
       })
       .addCase(getUser.fulfilled, (state, action: AnyAction) => {
         state.status = "succeeded";
         state.auth = action.payload;
       })
-      .addCase(getUser.rejected, (state, action) => {
+      .addCase(getUser.rejected, (state, action: AnyAction) => {
         state.status = "failed";
-        state.error = action.error.message as string | null;
+        if (action.payload) {
+          state.error = action.payload.errorMessage;
+        } else {
+          state.error = action.error.message;
+        }
+      })
+      .addCase(setLogout.pending, (state) => {
+        state.status = "pending";
       })
       .addCase(setLogout.fulfilled, (state, action: AnyAction) => {
         state.status = "idle";
@@ -104,4 +155,5 @@ const authSlice = createSlice({
 });
 
 export default authSlice.reducer;
+export const { resetError } = authSlice.actions;
 export const authenState = (state: RootState) => state.auth;
