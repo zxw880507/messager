6e380d0bc331e0ebda102ce2755253a747974892
Calvin <zxw880507@gmail.com>
2021-10-17 16:10:06 -0600

    create conversation slice

---
+++ store/features/conversationSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk, AnyAction } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootState } from "..";
+import type { Data } from "../../pages/api/conversation/[userId]";
+
+enum Status {
+  idle = "idle",
+  loading = "loading",
+  succeeded = "succeeded",
+  failed = "failed",
+}
+
+interface conversationState {
+  conversation: Data;
+  status: keyof typeof Status;
+  error: string | null | undefined;
+}
+const initialState: conversationState = {
+  conversation: [],
+  status: "idle",
+  error: null,
+};
+
+export const getConversation = createAsyncThunk(
+  "/getConversation",
+  async (id) => {
+    const res = await axios.get(`/api/conversation/${id}`);
+    return res.data;
+  }
+);
+
+const conversationSlice = createSlice({
+  name: "conversation",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getConversation.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getConversation.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(getConversation.fulfilled, (state, action: AnyAction) => {
+        state.conversation = action.payload;
+      });
+  },
+});
+
+export default conversationSlice.reducer;
+export const conversationState = (state: RootState) => state.conversation;
