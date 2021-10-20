fbc869bc056d6132e6a15c9c1128b4674c57b16e
Calvin <zxw880507@gmail.com>
2021-10-19 19:21:04 -0600

    create messages slice

---
+++ store/features/messagesSlice.ts
@@ -0,0 +1,59 @@
+import { AnyAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootState } from "..";
+import type { Messages } from "../../pages/api/message/[conversationId]";
+
+enum Status {
+  idle = "idle",
+  loading = "loading",
+  succeeded = "succeeded",
+  failed = "failed",
+}
+
+interface messageState {
+  messages: Messages;
+  status: keyof typeof Status;
+  error: string | null | undefined;
+}
+
+const initialState: messageState = {
+  messages: [],
+  status: "idle",
+  error: null,
+};
+
+export const getMessages = createAsyncThunk(
+  "/getMessages",
+  async (_, { getState, dispatch }) => {
+    const { chat } = getState() as RootState;
+    const res = await axios.get(`/api/message/${chat.conversationId}`);
+    return res.data;
+  }
+);
+
+export const sendMessage = createAsyncThunk("/sendMessage", async (_, {getState, dispatch}) => {
+  
+})
+
+const messagesSlice = createSlice({
+  name: "messages",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getMessages.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getMessages.fulfilled, (state, action: AnyAction) => {
+        state.status = "succeeded";
+        state.messages = action.payload;
+      })
+      .addCase(getMessages.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default messagesSlice.reducer;
+export const messagesState = (state: RootState) => state.messagesState;
