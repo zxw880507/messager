cedc8cfbadcd6c2e73589025cd82a48124dba642
Calvin <zxw880507@gmail.com>
2021-10-22 15:50:09 -0600

    remove all users

---
+++ prisma/reset.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+
+  await prisma.user.deleteMany({});
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
