b8b2b16521feef7ffa7cfd4e444bceb7d4f7b459
Calvin <zxw880507@gmail.com>
2021-10-26 17:23:46 -0600

    add state into element

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import style from "./Profile.module.scss";
 import { Avatar, Paper, Typography, IconButton } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SaveIcon from "@mui/icons-material/Save";
 import EditIcon from "@mui/icons-material/Edit";
 import CustomInput from "./CustomInput";
+import DateSelect from "./DatePicker";
 
 export default function Profile() {
+  const [editMode, setEditMode] = useState<boolean>(false);
   return (
     <div className={style.grid}>
       <div className={style.avatarBox}>
@@ -33,65 +38,92 @@ export default function Profile() {
       <div className={style.profileInfo}>
         <div className={style.infoHeader}>
           <Typography variant="h5">Basic Information</Typography>
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="span"
-          >
-            <EditIcon />
-          </IconButton>
+          {editMode ? (
+            <LoadingButton
+              sx={{
+                padding: ".2em .6em",
+                backgroundColor: "#8ca6db",
+                "&:hover": {
+                  backgroundColor: "#b993d6",
+                },
+              }}
+              onClick={() => setEditMode(false)}
+              // loading={loading}
+              loadingPosition="start"
+              startIcon={<SaveIcon />}
+              variant="contained"
+            >
+              Save
+            </LoadingButton>
+          ) : (
+            <IconButton
+              sx={{ color: "#8ca6db", padding: ".1em" }}
+              aria-label="upload picture"
+              component="span"
+              onClick={() => setEditMode(true)}
+            >
+              <EditIcon />
+            </IconButton>
+          )}
         </div>
         <Paper elevation={1} sx={{ width: "100%", height: "100%" }}>
           <ul>
-            {/* <li>
-              <span>Nickname</span>
-              <span>Shit Dragon</span>
-            </li>
-            <li>
-              <span>First Name</span>
-              <span>Jack</span>
-            </li>
-            <li>
-              <span>Last Name</span>
-              <span>Zhao</span>
-            </li>
-            <li>
-              <span>Date of Birth</span>
-              <span>N/A</span>
-            </li>
-            <li>
-              <span>Location</span>
-              <span>Calgary</span>
-            </li>
-            <li>
-              <span>Bio</span>
-              <span>I love cancer lady</span>
-            </li> */}
-
-            <li>
-              <span>Nickname</span>
-              <CustomInput />
-            </li>
-            <li>
-              <span>First Name</span>
-              <CustomInput />
-            </li>
-            <li>
-              <span>Last Name</span>
-              <CustomInput />
-            </li>
-            <li>
-              <span>Date of Birth</span>
-              <CustomInput />
-            </li>
-            <li>
-              <span>Location</span>
-              <CustomInput />
-            </li>
-            <li>
-              <span>Bio</span>
-              <CustomInput />
-            </li>
+            {/*  */}
+            {editMode ? (
+              <>
+                <li>
+                  <span>Nickname</span>
+                  <CustomInput />
+                </li>
+                <li>
+                  <span>First Name</span>
+                  <CustomInput />
+                </li>
+                <li>
+                  <span>Last Name</span>
+                  <CustomInput />
+                </li>
+                <li>
+                  <span>Date of Birth</span>
+                  <DateSelect />
+                </li>
+                <li>
+                  <span>Location</span>
+                  <CustomInput />
+                </li>
+                <li>
+                  <span>Bio</span>
+                  <CustomInput />
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <span>Nickname</span>
+                  <span>Shit Dragon</span>
+                </li>
+                <li>
+                  <span>First Name</span>
+                  <span>Jack</span>
+                </li>
+                <li>
+                  <span>Last Name</span>
+                  <span>Zhao</span>
+                </li>
+                <li>
+                  <span>Date of Birth</span>
+                  <span>N/A</span>
+                </li>
+                <li>
+                  <span>Location</span>
+                  <span>Calgary</span>
+                </li>
+                <li>
+                  <span>Bio</span>
+                  <span>I love cancer lady</span>
+                </li>
+              </>
+            )}
           </ul>
         </Paper>
       </div>
